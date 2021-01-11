a4410830e00392eaa408abf8bf98011f9c7e9f04
A_ShChe <65690436+sweschm19@users.noreply.github.com>
2021-01-11 21:40:15 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// let huck = 'innovate life';
+//
+//
+const innova_tech = 'texas';
+// innova_tech = "Hello world";
+const remove = [1,2,3,4]
+// console.log(remove[2]);
+//loop sytacx
+// let index = 0;
+// while(index < remove) {
+//     console.log(index[index]);
+//     index++;
+// } 
+// if (remove.length > 5) {
+//     console.log("huck");
+// } else {
+//     console.log("fuck");
+// }
+
+//function, Arrow function+
+const test = (arg) => {
+    if (remove.length > arg) {
+        console.log("huck");
+    } else {
+        console.log("fuck");
+    }   
+}
+// test(5);
+//Object
+const innova_tech2 = {
+    color: 'blue',
+    size: 'medium',
+    purfume:'rose',
+    deveLop: () => {
+        console.log('Chao Italy');
+    }
+};
+// console.log(innova_tech2.deveLop());
+//jsで、スマホサイズになったら、大きさを変える
+// console.log(window.innerWidth);
+// window.alert('Hello world!');
+document.getElementsByTagName('button')[1].addEventListener('click', ()=> {
+    window.alert('Chao Italy');
+});
+// console.log('button');
